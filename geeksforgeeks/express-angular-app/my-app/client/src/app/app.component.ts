1d8e9064d735d52186b4eb9b0d14a29815b16bd2
Srinivasa Rao Gurram <srinivasaraogurram@users.noreply.github.com>
2024-06-29 21:42:02 +0000

    update

--- geeksforgeeks/express-angular-app/my-app/client/src/app/app.component.ts
+++ geeksforgeeks/express-angular-app/my-app/client/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'client';
+import { Component, OnInit } from '@angular/core'; 
+import { ApiService } from './api.service'; 
+  
+@Component({ 
+    selector: 'app-root', 
+    templateUrl: './app.component.html', 
+    styleUrls: ['./app.component.css'] 
+}) 
+export class AppComponent implements OnInit { 
+    title = 'frontEnd'; 
+    message: any; 
+    constructor(private apiService: ApiService) { }; 
+    ngOnInit() { 
+        this.apiService.getMessage().subscribe(data => { 
+            this.message = data; 
+        }); 
+    } 
 }
